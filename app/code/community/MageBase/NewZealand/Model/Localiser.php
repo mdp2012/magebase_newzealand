1e829f0882074e11ba068ab23ce95f38ad1193ce
Kristof Ringleff <kristof@fooman.co.nz>
2012-09-27 11:05:07 +0200

    work with updated mdp_localiser

--- app/code/community/MageBase/NewZealand/Model/Localiser.php
+++ app/code/community/MageBase/NewZealand/Model/Localiser.php
@@ -1,13 +1,27 @@
 <?php
 
-class MageBase_NewZealand_Localiser implements Mdp2012_Localiser_Model_Interface
+class MageBase_NewZealand_Model_Localiser extends Mdp2012_Localiser_Model_Localiser_Abstract
 {
-    public function getSupportedLocaleCodes()
+
+    const CODE = 'magebase_newzealand';
+    const NAME = 'MageBase_NewZealand';
+
+    public function getCode()
     {
+        return self::CODE;
+    }
+
+    public function getModuleName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * return the locale code
+     * @return array
+     */
+    public function getSupportedLocaleCodes()    {
         return array('en_NZ');
     }
 
-    public function getTaxConfiguration($localeCode)
-    {
-    }
 }
